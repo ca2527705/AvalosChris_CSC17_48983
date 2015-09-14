06d25eb10bf652f7e229337b186f668a68f0c2c7
ca2527705 <cavalos15@student.rcc.edu>
2015-09-13 22:02:04 -0700

    Update

--- Homework/Assignment_1_Review/Problem_6.7/main.cpp
+++ Homework/Assignment_1_Review/Problem_6.7/main.cpp
@@ -20,17 +20,20 @@ float celsius(float);
 
 //Program starts here
 int main(int argc, char** argv) {
-    
-    float f,cel,C;
-    cin>>f;
-    cel=celsius(f);
-    //C=(5.0/9.0)*(f-32);
-    cout<<setprecision(2)<<fixed;
-    cout<<C<<endl;
+    //Verify the variable
+    float f,C;
+    //Create a loop for the function
+    for(int f=0;f<=20;f++){
+        //Call the function
+        float C=celsius(f);
+        //Display the results for every temp
+        cout<<setprecision(2)<<fixed;
+        cout<<f<<" Degres Fahrenheit = "<<C<<" Celsius"<<endl;
+    }
 
     return 0;
 }
-
+//Function for converting Fahrenheit to Celsius
 float celsius (float f){
     float C;
     C=(5.0/9.0)*(f-32);
