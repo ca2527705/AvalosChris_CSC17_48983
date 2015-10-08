63dffd6b66bf22bc16c1a2ab819b963d9086cd8e
Usatashi <Usatashi@yahoo.com>
2015-10-08 10:36:18 -0700

    Signed-off-by: Usatashi <Usatashi@yahoo.com>

--- Homework/Assingment_3/Problem_10.4/main.cpp
+++ Homework/Assingment_3/Problem_10.4/main.cpp
@@ -18,40 +18,50 @@ using namespace std;
 //Global Constants
 
 //Function Prototypes
-void wrdcount(char *,char);
-
+int wrdcount(char *,char);//function to find amount of words
 //Program starts here
 int main(int argc, char** argv) {
+    //Initialize the variables
     const int SIZE=101;
     char line[SIZE];
     char letter;
-    int count=0;
-    int length;
+    int count=0,length;
+    float word;
     
     cout<<"Enter a string up to 100 letters:\n";
     cin.getline(line,SIZE);
     
+    //Repeat what was entered to the user
     cout<<"This is what you entered:\n";
-    while(line[count]!='\0'){
-        cout<<line[count];
-        count++;           
-    }
-
-    wrdcount(line,SIZE);
+//    while(line[count]!='\0'){
+//        cout<<line[count];
+//        count++;           
+//    }
+    cout<<line<<endl;
     
-
+    //Send it to the two different functions
+    int temp=wrdcount(line,SIZE);
+    length=strlen(line);
+    word=float((length-temp)/float(temp));
     
+    
+    cout<<"There are "<<temp<<" words in the sentence"<<endl;
+    cout<<"There are an average of "<<word<<" letters per word"<<endl;   
 
     return 0;
 }
-
-void wrdcount(char *line,char SIZE){
-    int count=0;    
+//function used to find the amount of words in the entered sentence
+int wrdcount(char *line,char SIZE){
+    int count=0;
+    int length;
         for (int i=0;i<SIZE;i++){
         if (*line==' ')
             count++;
         line++;
         }
-    cout<<"\nThere are "<<count+1<<" words in the sentence"<<endl;
+    //cout<<"\nThere are "<<count<<" words in the sentence"<<endl; 
+    return count;
+
 }
+
 
